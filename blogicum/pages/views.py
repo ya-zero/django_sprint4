11b98b3cd6343f382daba93d554dba24911d1573
alexander voznyy <a.voznyy@vk.team>
2024-05-31 19:16:58 +0300

    fix error 500

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -9,5 +9,5 @@ def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
 
 
-def server_error(request):
+def error_500(request):
     return render(request, 'pages/500.html', status=500)
